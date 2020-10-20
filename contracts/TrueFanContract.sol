14e5f0c788fcd56b4cb9416065be62bb7795933a
Arshaan <arshaan.bhimani@uwaterloo.ca>
2020-10-19 22:02:59 -0400

    fixed 1st function's test, initial 2nd function, stuck on tests

--- contracts/TrueFanContract.sol
+++ contracts/TrueFanContract.sol
@@ -2,20 +2,58 @@ pragma solidity >=0.4.22 <0.8.0;
 
 contract TrueFanContract {
     struct Creator {
-        string username;
         string displayPictureURL;
-        address creatorAddress;
+        uint creatorAddress;
     }
 
-    // Every creator must have a unique username.
+    struct Subscription {
+        string creatorUsername;
+        uint lastPayment;
+    }
+
     mapping(string => Creator) public usernameToCreator;
+    mapping(uint => Subscription[]) public addressToSubscriptions;
 
     function registerCreator(
         string memory _username,
         string memory _displayPictureURL
     ) public {
-      require(usernameToCreator[_username].creatorAddress == address(0x0));
-      Creator memory newCreator = Creator(_username, _displayPictureURL, msg.sender);
-      usernameToCreator[_username] = newCreator;
+        require(
+            address(usernameToCreator[_username].creatorAddress) ==
+                address(0x0),
+            "The username must be unique."
+        );
+        Creator memory newCreator = Creator(
+            _displayPictureURL,
+            uint(msg.sender)
+        );
+        usernameToCreator[_username] = newCreator;
+    }
+
+    function purchaseSubscription(string memory _creatorUsername) public {
+        require(
+            address(usernameToCreator[_creatorUsername].creatorAddress) !=
+                address(0x0),
+            "The username must exist."
+        );
+
+        Subscription memory foundSubscription;
+        Subscription[] memory allSubscriptions = addressToSubscriptions[uint(msg
+            .sender)];
+        for (uint i = 0; i < allSubscriptions.length; i++) {
+            Subscription memory currSub = allSubscriptions[i];
+            if (
+                keccak256(abi.encodePacked(currSub.creatorUsername)) ==
+                keccak256(abi.encodePacked(_creatorUsername))
+            ) {
+                foundSubscription = currSub;
+                break;
+            }
+        }
+
+        if (bytes(foundSubscription.creatorUsername).length == 0) {
+            foundSubscription = Subscription(_creatorUsername, block.timestamp);
+            addressToSubscriptions[uint(msg.sender)].push(foundSubscription);
+        }
     }
 }
--- test/Test-TrueFanContract.js
+++ test/Test-TrueFanContract.js
@@ -1,11 +1,10 @@
-const chai = require('chai');
-chai.use(require('chai-as-promised'));
+const chai = require("chai");
+chai.use(require("chai-as-promised"));
 const { expect } = chai;
 
 const TrueFanContract = artifacts.require("TrueFanContract");
 
 contract("TrueFanContract", (accounts) => {
-  // beforeAll(async () => {});
   const names = ["Alice", "Bob", "Charlie"];
   const URLs = [
     "https://static.wikia.nocookie.net/fairlyoddfanon/images/e/ed/Cosmo_image.png/revision/latest/top-crop/width/300/height/300?cb=20200709205901",
@@ -13,14 +12,43 @@ contract("TrueFanContract", (accounts) => {
     "https://vignette.wikia.nocookie.net/dpwikia/images/3/3c/Render-_Danny_Phantom_stock_image.png/revision/latest?cb=20200412001659",
   ];
 
-  it("should fail if Creator exists", async () => {
+  xit("should add new Creator", async () => {
     const TrueFanContractInstance = await TrueFanContract.deployed();
     await TrueFanContractInstance.registerCreator(names[0], URLs[0], {
       from: accounts[0],
     });
 
     expect(
-      TrueFanContractInstance.registerCreator(names[0], URLs[1])
+      (await TrueFanContractInstance.usernameToCreator(names[0]))
+        .displayPictureURL
+    ).to.equal(URLs[0]);
+  });
+
+  xit("should fail if Creator exists", async () => {
+    const TrueFanContractInstance = await TrueFanContract.deployed();
+    await TrueFanContractInstance.registerCreator(names[1], URLs[0], {
+      from: accounts[1],
+    });
+
+    expect(TrueFanContractInstance.registerCreator(names[1], URLs[1])).to.be
+      .rejected;
+  });
+
+  xit("should fail if no such Creator exists", async () => {
+    const TrueFanContractInstance = await TrueFanContract.deployed();
+
+    expect(
+      TrueFanContractInstance.purchaseSubscription(names[0], {
+        from: accounts[1],
+      })
     ).to.be.rejected;
+  });
+
+  it("should add a new subscription", async () => {
+    const TrueFanContractInstance = await TrueFanContract.deployed();
+
+    console.log(
+      await TrueFanContractInstance.addressToSubscriptions(accounts[0])
+    );
   });
 });
